48c969f34229e6f91028e4d18461aefb90d4d34c
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-13 14:47:34 -0400

    did it with kevin and sam

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,13 +7,53 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    index: 0,
+    eatenSushi:[],
+    wallet: 100
+  }
+
+  sushiFour = () => {
+    return this.state.sushi.slice(this.state.index, this.state.index + 4)
+  }
+
+  moreSushi = () => {
+    let newIndex = this.state.index + 4
+    this.setState({
+      index: newIndex
+    })
+  }
+
+  ateSushi = (props) => {
+    if(props.price >= this.state.wallet) {
+      null
+    } else {
+      this.setState({
+       eatenSushi: [...this.state.eatenSushi, props],
+       wallet: this.state.wallet - props.price 
+      })
+    }
+  }
+
   render() {
+
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer list={this.sushiFour()} moreSushi={this.moreSushi} ateSushi={this.ateSushi} eatenSushi={this.state.eatenSushi}/>/>
+        <Table eatenSushi={this.state.eatenSushi} wallet={this.state.wallet}/>
       </div>
     );
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(listOfSushi => {
+      this.setState({
+        sushi: listOfSushi
+      })
+    })
   }
 }
 
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import MoreButton from '../components/MoreButton';
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  
+  const renderSushi = () => {
+    return props.list.map(sushi => <Sushi key={sushi.id} sushi={sushi} ateSushi={props.ateSushi} eatenSushi={props.eatenSushi}/>)
+  } 
+  
+  
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {renderSushi()}
+        <MoreButton moreSushi={props.moreSushi}/>
       </div>
     </Fragment>
   )
